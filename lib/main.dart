a77327d684f2b1100e2f18f4bd9764dad6f080bb
Ajay Choudhary <achoudhary@konradgroup.com>
2026-03-16 15:59:39 +0530

    Test github workflow

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: DSTheme.lightTheme,
+      // theme: DSTheme.lightTheme,
       home: DemoScreen(),
     );
   }
